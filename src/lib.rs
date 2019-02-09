aba43e079697cbe303fabd93aeab45abd65d0e8f
CPerezz <c.perezbaro@gmail.com>
2019-02-09 22:56:23 +0100

    Defining lib skeleton and basic functions

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+extern crate num;
+extern crate rand;
+extern crate num_bigint;
+
+pub mod helpers;
+
+
+pub fn generate_key_pair() {
+    
+}
+
+pub fn encrypt(){
+
+}
+
+pub fn decrypt(){
+
+}
+
+pub fn sign_message() {
+
+}
