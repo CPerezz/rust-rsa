aaa29cf0e58a2df4cccd7cd5bfae3554ad73a9c5
CPerezz <c.perezbaro@gmail.com>
2019-02-09 22:58:17 +0100

    Implemented gen big rand num

---
+++ src/helpers/math.rs
@@ -0,0 +1,47 @@
+//! Math functions to build keys with trusted primes
+use rand;
+use num_bigint::{ToBigUint, BigUint, RandBigInt};
+use num::{Zero, One, Integer};
+
+// Generates a big number of lenght = u32 param.
+pub fn gen_big_num(bit_len: &u32) -> BigUint {
+    // RNG depends on rng_core crate.
+    let mut rng = rand::thread_rng();
+    let mut a = rng.gen_biguint(bit_len.to_owned() as usize);
+    a
+}
+
+// Given lenght, generates a prime number of that lenght approximately. 
+pub fn gen_big_prime(size: &u32) -> BigUint {
+    let mut proposal =  gen_big_num(size);
+    // Remove all even numbers to reduce the iterations a half.
+    if proposal.is_even() {
+        proposal = proposal + BigUint::one();
+    }
+    while !is_prime(&proposal) {
+        // Steps of 2 to avoid the even numbers on the iterations.
+        proposal =  proposal + 2.to_biguint().unwrap();
+    }
+    proposal
+}
+
+// Given a prime proposal, compute Rabin Miller's algorithm.
+fn is_prime(proposal: &BigUint) -> bool {
+    if !rabin_miller(proposal) {return false}
+    true
+}
+
+// Rabin-Miller is a probabilistic algorithm that checks if a number is prime based on Riemmann's conjecture.
+fn rabin_miller(proposal: &BigUint) -> bool {
+
+
+
+    true
+}
+
+
+#[test]
+fn generates_random_biguint() {
+    let a = gen_big_num(&1024);
+    assert_ne!(a, BigUint::zero());
+}
