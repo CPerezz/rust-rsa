d8f18a05ab2e16dd234baf0fe5d9e044c18f881e
CPerezz <c.perezbaro@gmail.com>
2019-02-10 20:46:22 +0100

    Defining future structure

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,32 @@ extern crate rand;
 extern crate num_bigint;
 
 pub mod helpers;
+pub mod types;
 
+use crate::types::{KeyPair, Threshold};
+use num_bigint::BigUint;
 
-pub fn generate_key_pair() {
-    
+pub fn generate_key_pair(size: &u32, threshold: &Threshold) -> KeyPair {
+    // Probably will be implemented for KeyPair Struct
+  unimplemented!()
+}
+
+pub fn generate_key_pair_from_p_q(p: &BigUint, q: &BigUint) -> Result<KeyPair, &'static str> {
+    // Probably will be implemented for KeyPair Struct
+ unimplemented!()
 }
 
 pub fn encrypt(){
-
+    // Probably will be implemented for PublicKey Struct
+unimplemented!()
 }
 
 pub fn decrypt(){
-
+    // Probably will be implemented for SecretKey Struct
+unimplemented!()
 }
 
 pub fn sign_message() {
-
+    // Probably will be implemented for SecretKey Struct
+unimplemented!()
 }
