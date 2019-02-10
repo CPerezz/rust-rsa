f85ed7cc7da0c98b67478dc7547d36e590bd4c44
CPerezz <c.perezbaro@gmail.com>
2019-02-10 20:46:51 +0100

    Create Threshold Struct and default() static method.

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,7 @@
 use num_bigint::{BigUint};
+use crate::helpers::math::*;
 
-pub struct Key_Pair {
+pub struct KeyPair {
     pub pk: PublicKey,
     pub sk: SecretKey,
     pub size: u32
@@ -15,3 +16,35 @@ pub struct PublicKey {
     n: BigUint,
     e: BigUint
 }
+
+pub struct Threshold {
+    value: u32
+}
+
+impl Threshold {
+    // Creates a Threshold with a default error probability of generating a prime of 4^-64
+    pub fn default() -> Self {
+        let threshold = Threshold {
+            value: 64 as u32
+        };
+        threshold
+    }
+}
+
+/*
+impl KeyPair {
+    fn new(size: &u32, threshold: &Threshold) -> Result<Self, &'static str> {
+        let p = gen_big_prime(size, threshold.value);
+        let q = gen_big_prime(size, threshold.value);
+        let n = ecc(&p, &q);
+    }
+}
+*/
+
+/*
+impl PublicKey {
+    pub fn new(size: &u32) -> Result<Self, &'static str> {
+        
+    }
+}
+*/
