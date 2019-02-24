d38d2ba5dc0131c1b839a1341405b8c8ee47054d
CPerez19 <c.perezbaro@gmail.com>
2019-02-24 09:57:21 +0100

    Fix tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,5 @@
-extern crate rsa_rust;
+
+
 
 use rsa_rust::helpers::math::*;
 use rsa_rust::types::*;
@@ -8,8 +9,8 @@ use rsa_rust::types::*;
 fn generates_random_biguint() {
     let a = gen_big_num(&1024);
     assert_eq!(a.to_bytes_be().len(), 128 as usize);
-    let b = gen_big_num(&2056);
-    assert_eq!(b.to_bytes_be().len(), 257 as usize);
+    let b = gen_big_num(&2048);
+    assert_eq!(b.to_bytes_be().len(), 256 as usize);
     let c = gen_big_num(&512);
     assert_eq!(c.to_bytes_be().len(), 64 as usize);
     let d = gen_big_num(&4096);
@@ -32,7 +33,6 @@ fn encrypts_decrypts_info() {
     let kp = KeyPair::new(&512u32, &Threshold::new(&10)).unwrap();
     let msg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non nunc et ipsum tempus fermentum";
     let cyphertext = kp.pk.encrypt(msg).unwrap();
-    
 
     let res_decrypt = kp.sk.decrypt(&cyphertext).unwrap();
     println!("Result of decryption is: {}", res_decrypt);
